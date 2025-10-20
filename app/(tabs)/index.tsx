cb68d91f5ac327c2a0605855e92a40796907a966
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-10-19 23:31:30 -0500

    feat: add ternding movies section with real-time updates.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,21 +5,32 @@ import { images } from "@/constants/images";
 import useFetch from "@/hooks/useFetch";
 import { fetchMovies } from "@/services/api";
 import { useRouter } from "expo-router";
+import { getTrendingMovies } from "../../services/appwrite";
 
 import MovieCard from "@/components/MovieCard";
+import TrendingCard from "@/components/TrendingCard";
 
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 /**
  * Index Screen 
  * @summary
- * Displays a list of latest movies with a search bar.
+ * Displays a list of latest movies with a search bar and trending movies section.
  * 
  * @returns The rendered Index screen component.
  */
 export default function Index() {
 
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+    refetch: refetchTrending
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -29,18 +40,16 @@ export default function Index() {
     query: ''
   }));
 
-  const renderHeader = () => (
-    <View className="px-5 flex-col gap-2 mt-20">
-      <Image source={icons.logo} className="w-12 h-10 mb-5 mx-auto" />
-      <SearchBar
-        onPress={() => router.push("/search")}
-        placeholder="Search for a movie"
-      />
-      <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
-    </View>
+  /**
+   * Use focus effect to refetch trending movies when the screen is focused.
+   */
+  useFocusEffect(
+    useCallback(() => {
+      refetchTrending();
+    }, [refetchTrending])
   );
 
-  if (moviesLoading) {
+  if (moviesLoading || trendingLoading) {
     return (
       <View className="flex-1 bg-primary justify-center">
         <Image source={images.bg} className="absolute w-full z-0" />
@@ -53,12 +62,12 @@ export default function Index() {
     );
   }
 
-  if (moviesError) {
+  if (moviesError || trendingError) {
     return (
       <View className="flex-1 bg-primary justify-center px-5">
         <Image source={images.bg} className="absolute w-full z-0" />
         <Text className="text-white text-center text-lg">
-          Error: {moviesError?.message}
+          Error: {moviesError?.message || trendingError?.message || "Something went wrong"}
         </Text>
       </View>
     );
@@ -70,7 +79,37 @@ export default function Index() {
 
       <FlatList
         data={movies}
-        ListHeaderComponent={renderHeader}
+        ListHeaderComponent={
+          <View className="px-5 flex-col gap-2 mt-20">
+            <Image source={icons.logo} className="w-12 h-10 mb-5 mx-auto" />
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+
+            {trendingMovies && trendingMovies.length > 0 && (
+              <View className="mt-5">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  data={trendingMovies}
+                  contentContainerStyle={{
+                    gap: 26,
+                  }}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.$id}
+                />
+              </View>
+            )}
+
+            <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+          </View>
+        }
         renderItem={({ item }) => <MovieCard {...item} />}
         keyExtractor={(item) => item.id.toString()}
         numColumns={3}
